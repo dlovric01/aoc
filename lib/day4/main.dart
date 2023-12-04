07e64d924b562c55fad9dc2997a7cdf8002115a3
dlovric01 <danijel.lovric@valeremargins.com>
2023-12-04 16:00:47 +0100

    day4: part2

--- lib/day4/main.dart
+++ lib/day4/main.dart
@@ -3,8 +3,10 @@ import 'package:aoc/read_input.dart';
 void main(List<String> args) {
   readInput('lib/day4/input.txt').then((contents) {
     List<String> games = contents.split('\n');
+    int p1 = 0;
+    int p2 = 0;
 
-    int sum = 0;
+    var listOfCards = List.generate(games.length, (index) => 1);
 
     for (var i = 0; i < games.length; i++) {
       var cardValue = 1;
@@ -30,9 +32,23 @@ void main(List<String> args) {
         cardValue = 0;
       }
 
-      sum = sum + cardValue;
-    }
+      p1 = p1 + cardValue;
 
-    print(sum);
+      final rep = listOfCards[i];
+      for (var c = 0; c < rep; c++) {
+        for (var v = 0; v < matchingNumbersLenght; v++) {
+          try {
+            listOfCards[i + v + 1]++;
+          } catch (e) {
+            break;
+          }
+        }
+      }
+    }
+    p2 = listOfCards.fold(
+        0, (previousValue, element) => previousValue + element);
+
+    print(p1);
+    print(p2);
   });
 }
