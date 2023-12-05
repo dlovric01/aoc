b06a781ce88618d315a1b37a0f23f64f90c01597
dlovric01 <danijel.lovric@valeremargins.com>
2023-12-05 22:15:23 +0100

    fix: final code

--- lib/day5/main.dart
+++ lib/day5/main.dart
@@ -1,44 +1,39 @@
 import 'dart:io';
+import 'dart:async';
 
-void main() {
-  final input = File('lib/day5/input.txt').readAsStringSync();
-  List<String> lines = input.split('\n');
-  final seeds = lines.first.split(':')[1].trim().split(' ');
+List<List<List<int>>> mapRanges = List.generate(7, (index) => []);
 
-  List<int> values = [];
-
-  for (var i = 0; i < seeds.length; i++) {
-    var value = int.parse(seeds[i]);
-
-    for (var j = 1; j < lines.length; j++) {
-      final line = lines[j];
-
-      if (line.isEmpty || !line.isDigit) {
-        continue;
-      } else {
-        final soils = line.split(' ');
-        if (value >= int.parse(soils[1]) &&
-            value <= (int.parse(soils[1]) + int.parse(soils[2]))) {
-          final sum = int.parse(soils[0]) - int.parse(soils[1]);
-          value = value + sum;
-          for (var k = j; k < lines.length; k++) {
-            if (lines[k].isEmpty) {
-              j = k;
-              break;
-            }
-          }
-        } else {
-          continue;
-        }
-      }
+int get(int depth, int src) {
+  for (List<int> range in mapRanges[depth]) {
+    if (range[0] <= src && src < range[1]) {
+      int newSrc = src - range[0] + range[2];
+      return depth == 6 ? newSrc : get(depth + 1, newSrc);
     }
-    values.add(value);
   }
-  print(values.reduce((curr, next) => curr < next ? curr : next));
+  return depth == 6 ? src : get(depth + 1, src);
 }
 
-extension on String {
-  bool get isDigit {
-    return RegExp(r'\d').hasMatch(this);
+Future<void> main() async {
+  String content = await File('lib/day5/input.txt').readAsString();
+  List<String> ls = content.split('\n\n');
+
+  List<int> seeds = ls[0].split(': ')[1].split(' ').map(int.parse).toList();
+  List<List<List<int>>> maps = ls
+      .sublist(1)
+      .map((l) => l
+          .split(':\n')[1]
+          .split('\n')
+          .map((n) => n.split(' ').map(int.parse).toList())
+          .toList())
+      .toList();
+
+  for (int depth = 0; depth < maps.length; depth++) {
+    for (List<int> m in maps[depth]) {
+      mapRanges[depth].add([m[1], m[1] + m[2], m[0]]);
+    }
   }
+
+  print(seeds
+      .map((seed) => get(0, seed))
+      .reduce((value, element) => value < element ? value : element));
 }
