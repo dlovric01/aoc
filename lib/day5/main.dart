37333caf17e235b4c571a218f450b8281c733ac2
dlovric01 <danijel.lovric@valeremargins.com>
2023-12-06 20:33:43 +0100

    day5: part 1

--- lib/day5/main.dart
+++ lib/day5/main.dart
@@ -1,24 +1,11 @@
 import 'dart:io';
-import 'dart:async';
 
-List<List<List<int>>> mapRanges = List.generate(7, (index) => []);
-
-int get(int depth, int src) {
-  for (List<int> range in mapRanges[depth]) {
-    if (range[0] <= src && src < range[1]) {
-      int newSrc = src - range[0] + range[2];
-      return depth == 6 ? newSrc : get(depth + 1, newSrc);
-    }
-  }
-  return depth == 6 ? src : get(depth + 1, src);
-}
-
-Future<void> main() async {
+void main(List<String> args) async {
   String content = await File('lib/day5/input.txt').readAsString();
-  List<String> ls = content.split('\n\n');
+  final ls = content.trim().split('\n\n');
 
-  List<int> seeds = ls[0].split(': ')[1].split(' ').map(int.parse).toList();
-  List<List<List<int>>> maps = ls
+  final seeds = ls[0].split(': ')[1].split(' ').map(int.tryParse).toList();
+  final maps = ls
       .sublist(1)
       .map((l) => l
           .split(':\n')[1]
@@ -27,13 +14,24 @@ Future<void> main() async {
           .toList())
       .toList();
 
-  for (int depth = 0; depth < maps.length; depth++) {
-    for (List<int> m in maps[depth]) {
-      mapRanges[depth].add([m[1], m[1] + m[2], m[0]]);
-    }
-  }
+  final results = <int>[];
 
-  print(seeds
-      .map((seed) => get(0, seed))
-      .reduce((value, element) => value < element ? value : element));
+  for (var i = 0; i < seeds.length; i++) {
+    var seed = seeds[i];
+    for (var j = 0; j < maps.length; j++) {
+      final map = maps[j];
+      for (var k = 0; k < maps[j].length; k++) {
+        final list = map[k];
+
+        if (seed! >= list[1] && seed < list[1] + list[2]) {
+          seed = seed + list[0] - list[1];
+          break;
+        } else {
+          continue;
+        }
+      }
+    }
+    results.add(seed!);
+  }
+  print(results.reduce((value, element) => value < element ? value : element));
 }
